714e4633602a3ad5481af9901e4ff1d4166d5f30
leeminseon <leeminseon03@gmail.com>
2025-09-23 09:13:57 +0900

    0923_lms

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/html', express.static(path.join(__dirname, 'html')));
+app.use('/components', express.static(path.join(__dirname, 'components')));
+
+
+// 회원가입
+app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname, "html", "signup.html"));
+});
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
