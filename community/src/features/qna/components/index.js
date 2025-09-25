88067d905b01c63699cb0fb183c14c9e26d4d7c5
Yohaaaan <132324260+Yohaaaan@users.noreply.github.com>
2025-09-25 14:09:24 +0900

    커뮤니티 초안 삭제

--- community/src/features/qna/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as QuestionCard } from "./QuestionCard";
-export { default as Tag } from "./Tag";
-export { default as Pagination } from "./Pagination";
-export { default as ExpertCard } from "./ExpertCard";
--- community/src/features/qna/pages/QuestionDetail.jsx
+++
@@ -1,3 +0,0 @@
-export default function QuestionDetail() {
-    return <div style={{ padding: 20 }}>Question Detail Page</div>;
-}
